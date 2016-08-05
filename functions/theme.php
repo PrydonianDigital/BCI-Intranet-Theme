91048cefeddd76fa5e840af8335618aaa05b6bba
vbloke <vbloke@gmail.com>
2016-08-05 12:23:09 +0100

    new icons

--- functions/theme.php
+++ functions/theme.php
@@ -347,3 +347,8 @@
 		return '<i class="nav-calendar"></i> Export to Calendar';
 	}
 
+	function my_myme_types($mime_types){
+		$mime_types['json'] = 'application/json';
+		return $mime_types;
+	}
+	add_filter('upload_mimes', 'my_myme_types', 1, 1);
