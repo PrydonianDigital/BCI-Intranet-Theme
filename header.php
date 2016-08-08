868a1dd67c688d27057a97d8c7afcbd89a199a66
vbloke <vbloke@gmail.com>
2016-08-08 10:22:57 +0100

    change to dashboard icon show/hide

--- header.php
+++ header.php
@@ -105,7 +105,7 @@
 			<a href="#"><i class="nav-menu"></i></a>
 		</div>
 
-		<div id="apps" class="<?php if(current_user_can('create_posts')) { ?>wide<?php } ?>">
+		<div id="apps" class="<?php if(current_user_can('edit_others_pages')) { ?>wide<?php } ?>">
 			<?php if(current_user_can('activate_plugins')) { ?>
 				<a class="appsButton close dash" id="dash"  href="<?php echo admin_url(); ?>" title="Dashboard"><i class="nav-equalizer2"></i></a>
 			<?php } ?>
