f756d597cf44ce8bdd298f99d682cb007e264095
vbloke <vbloke@gmail.com>
2016-08-17 16:15:04 +0100

    new connections

--- functions/p2p.php
+++ functions/p2p.php
@@ -4,7 +4,14 @@ function my_connection_types() {
 	if ( !function_exists('p2p_register_connection_type') )
 		return;
 
-	p2p_register_connection_type( array('name' => 'p2p', 'from' => 'page', 'to' => 'post' ) );
+	p2p_register_connection_type( array('name' => 'd2p', 'from' => 'doc', 'to' => 'page' ) );
+	p2p_register_connection_type( array('name' => 'd2p2', 'from' => 'doc', 'to' => 'post' ) );
+	p2p_register_connection_type( array('name' => 'd2e', 'from' => 'doc', 'to' => 'tribe_events' ) );
+	p2p_register_connection_type( array('name' => 'p2l', 'from' => 'page', 'to' => 'link' ) );
+	p2p_register_connection_type( array('name' => 'p2l2', 'from' => 'post', 'to' => 'link' ) );
+	p2p_register_connection_type( array('name' => 'n2l', 'from' => 'notice', 'to' => 'link' ) );
+	p2p_register_connection_type( array('name' => 'e2l', 'from' => 'tribe_events', 'to' => 'link' ) );
+	p2p_register_connection_type( array('name' => 'p2p', 'from' => 'tribe_events', 'to' => 'post' ) );
 	p2p_register_connection_type( array('name' => 'p2e', 'from' => 'page', 'to' => 'tribe_events' ) );
 	p2p_register_connection_type( array('name' => 'p2a', 'from' => 'page', 'to' => 'announcement' ) );
 	p2p_register_connection_type( array('name' => 'p2c', 'from' => 'page', 'to' => 'centre' ) );
