ad30a712ee31538d56a25c2751f6d1a80ef4949c
vbloke <vbloke@gmail.com>
2016-08-17 16:13:21 +0100

    added types to related stuff

--- related/related.php
+++ related/related.php
@@ -1,25 +1,163 @@
 <div class="row">
 
 	<?php
-		$members = get_post_meta( get_the_ID(), '_link_link', true );
-		$output = '';
-		if ( empty( $members ) ) {
-			echo $output;
-		} else {
-			$output .= '<div class="large-6 small-12 columns">';
-			$output .= '<h2>Related Links</h2>';
-			$output .= '<ul class="links">';
-			foreach ( $members as $member ) {
-				$output .= '<li>';
-				$output .= '<a href="'. esc_html( $member['url'] ) .'" target="_blank">';
-				$output .= esc_html( $member['title'] );
-				$output .= '</a>';
-				$output .= '</li>';
-			}
-			$output .= '</ul>';
-			$output .= '</div>';
-			echo $output;
+	$d2p = new WP_Query( array(
+		'connected_type' => 'd2p',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $d2p->have_posts() ) : ?>
+		<div class="large-6 small-12 columns">
+		<h2>Related Documents</h2>
+		<ul class="doc">
+	<?php
+		while ( $d2p->have_posts() ) : $d2p->the_post();
+		$entries = get_post_meta( get_the_ID(), 'doc_version', true );
+		foreach ((array) $entries as $key => $entry) {
+			$doc = '';
+			if ( isset( $entry['_doc'] ) )
+				$doc = esc_html( $entry['_doc'] );
+				if(end($entries) == $entry) {
+					echo '<li><a href="' . $doc . '">' . get_the_title() . '</a></li>';
+				}
 		}
+	endwhile; ?>
+		</ul>
+	</div>
+	<?php
+	endif; wp_reset_postdata();
+	?>
+
+	<?php
+	$d2p2 = new WP_Query( array(
+		'connected_type' => 'd2p2',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $d2p2->have_posts() ) : ?>
+		<div class="large-6 small-12 columns">
+		<h2>Related Documents</h2>
+		<ul class="doc">
+	<?php
+		while ( $d2p2->have_posts() ) : $d2p2->the_post();
+		$entries = get_post_meta( get_the_ID(), 'doc_version', true );
+		foreach ((array) $entries as $key => $entry) {
+			$doc = '';
+			if ( isset( $entry['_doc'] ) )
+				$doc = esc_html( $entry['_doc'] );
+				if(end($entries) == $entry) {
+					echo '<li><a href="' . $doc . '">' . get_the_title() . '</a></li>';
+				}
+		}
+	endwhile; ?>
+		</ul>
+	</div>
+	<?php
+	endif; wp_reset_postdata();
+	?>
+
+	<?php
+	$d2e = new WP_Query( array(
+		'connected_type' => 'd2e',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $d2e->have_posts() ) : ?>
+		<div class="large-6 small-12 columns">
+		<h2>Related Documents</h2>
+		<ul class="doc">
+	<?php
+		while ( $d2e->have_posts() ) : $d2e->the_post();
+		$entries = get_post_meta( get_the_ID(), 'doc_version', true );
+		foreach ((array) $entries as $key => $entry) {
+			$doc = '';
+			if ( isset( $entry['_doc'] ) )
+				$doc = esc_html( $entry['_doc'] );
+				if(end($entries) == $entry) {
+					echo '<li><a href="' . $doc . '">' . get_the_title() . '</a></li>';
+				}
+		}
+	endwhile; ?>
+		</ul>
+	</div>
+	<?php
+	endif; wp_reset_postdata();
+	?>
+
+	<?php
+	$p2l = new WP_Query( array(
+		'connected_type' => 'p2l',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $p2l->have_posts() ) : ?>
+	<div class="large-6 small-12 columns">
+		<h2>Related Links</h2>
+		<ul class="links">
+		<?php while ( $p2l->have_posts() ) : $p2l->the_post(); ?>
+			<li><a href="<?php $link = get_post_meta(get_the_ID(), 'link_url', true); echo $link; ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		</ul>
+	</div>
+	<?php wp_reset_postdata();
+	endif;
+	?>
+
+	<?php
+	$p2l2 = new WP_Query( array(
+		'connected_type' => 'p2l2',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $p2l2->have_posts() ) : ?>
+	<div class="large-6 small-12 columns">
+		<h2>Related Links</h2>
+		<ul class="links">
+		<?php while ( $p2l2->have_posts() ) : $p2l2->the_post(); ?>
+			<li><a href="<?php $link = get_post_meta(get_the_ID(), 'link_url', true); echo $link; ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		</ul>
+	</div>
+	<?php wp_reset_postdata();
+	endif;
+	?>
+
+	<?php
+	$n2l = new WP_Query( array(
+		'connected_type' => 'n2l',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $n2l->have_posts() ) : ?>
+	<div class="large-6 small-12 columns">
+		<h2>Related Links</h2>
+		<ul class="links">
+		<?php while ( $n2l->have_posts() ) : $n2l->the_post(); ?>
+			<li><a href="<?php $link = get_post_meta(get_the_ID(), 'link_url', true); echo $link; ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		</ul>
+	</div>
+	<?php wp_reset_postdata();
+	endif;
+	?>
+
+	<?php
+	$e2l = new WP_Query( array(
+		'connected_type' => 'e2l',
+		'connected_items' => $post,
+		'nopaging' => true,
+	) );
+	if ( $e2l->have_posts() ) : ?>
+	<div class="large-6 small-12 columns">
+		<h2>Related Links</h2>
+		<ul class="links">
+		<?php while ( $e2l->have_posts() ) : $e2l->the_post(); ?>
+			<li><a href="<?php $link = get_post_meta(get_the_ID(), 'link_url', true); echo $link; ?>"><?php the_title(); ?></a></li>
+		<?php endwhile; ?>
+		</ul>
+	</div>
+	<?php wp_reset_postdata();
+	endif;
 	?>
 
 	<?php
