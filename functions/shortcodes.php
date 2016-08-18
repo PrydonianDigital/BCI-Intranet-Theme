8c73426ef6f0fa02b8252a80c6db366f84c2310c
vbloke <vbloke@gmail.com>
2016-08-18 10:29:16 +0100

    add document to editor shortcode

--- functions/shortcodes.php
+++ functions/shortcodes.php
@@ -17,3 +17,32 @@
 	    return $form;
 
 	}
+
+	// tile shortcode
+	function show_docs($atts){
+	   extract(shortcode_atts(array(
+		  'p'					=> '',
+	   ), $atts));
+		$args = array (
+			'p'					=> $p,
+			'post_type'			=> array( 'doc' ),
+			'posts_per_page'	=> '1',
+		);
+		$query = new WP_Query( $args );
+		if ($query->have_posts()) :
+			while ($query->have_posts()) : $query->the_post();
+			$entries = get_post_meta( get_the_ID(), 'doc_version', true );
+			foreach ((array) $entries as $key => $entry) {
+				$doc = '';
+				if ( isset( $entry['_doc'] ) )
+					$doc = esc_html( $entry['_doc'] );
+					if(end($entries) == $entry) {
+						echo '<a href="' . $doc . '">' . get_the_title() . '</a>';
+					}
+			}
+			endwhile;
+		endif;
+		wp_reset_query();
+		return $return_string;
+	}
+	add_shortcode( 'doc','show_docs' );
