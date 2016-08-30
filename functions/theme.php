ce699edee89062d287066a3ccbac8984e42b827b
vbloke <vbloke@gmail.com>
2016-08-30 14:54:14 +0100

    multi post thumbnails/changed 'Posts' to 'News'

--- functions/theme.php
+++ functions/theme.php
@@ -10,7 +10,7 @@
 		add_image_size( 'header', '1172', '200', true );
 		add_image_size( 'display', '300', '250', true );
 		add_image_size( 'thumbnail-news', '227', '108', true );
-		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list', 'gallery', 'caption' ) );
+		add_theme_support( 'html5', array( 'search-form', 'comment-form', 'comment-list' ) );
 		add_theme_support( 'title-tag' );
 		remove_action( 'wp_head', 'wp_generator' );
 		remove_action( 'wp_head', 'rsd_link' );
@@ -27,6 +27,44 @@
 		));
 	}
 	add_action( 'after_setup_theme', 'bcc_theme' );
+
+	if (class_exists('MultiPostThumbnails')) {
+	    new MultiPostThumbnails(
+	        array(
+	            'label' => 'Square Feed Featured Image',
+	            'id' => 'secondary-image',
+	            'post_type' => 'post'
+	        )
+	    );
+	}
+
+	function bci_news_label() {
+	    global $menu;
+	    global $submenu;
+	    $menu[5][0] = 'News';
+	    $submenu['edit.php'][5][0] = 'News';
+	    $submenu['edit.php'][10][0] = 'Add News';
+	    $submenu['edit.php'][16][0] = 'News Tags';
+	}
+	function bci_news_object() {
+	    global $wp_post_types;
+	    $labels = &$wp_post_types['post']->labels;
+	    $labels->name = 'News';
+	    $labels->singular_name = 'News';
+	    $labels->add_new = 'Add News';
+	    $labels->add_new_item = 'Add News';
+	    $labels->edit_item = 'Edit News';
+	    $labels->new_item = 'News';
+	    $labels->view_item = 'View News';
+	    $labels->search_items = 'Search News';
+	    $labels->not_found = 'No News found';
+	    $labels->not_found_in_trash = 'No News found in Trash';
+	    $labels->all_items = 'All News';
+	    $labels->menu_name = 'News';
+	    $labels->name_admin_bar = 'News';
+	}
+	add_action( 'admin_menu', 'bci_news_label' );
+	add_action( 'init', 'bci_news_object' );
 
 	function bci_editor_styles() {
 		add_editor_style( 'bci-editor-style.css' );
@@ -70,7 +108,7 @@
 
 	add_action('admin_head', 'my_custom_fonts');
 	function my_custom_fonts() {
-		echo '<style> .menu-item-edit-active .menu-item-settings { overflow: hidden !important; } .cmb2_select { font-family: "nav"; } </style>';
+		echo '<style> .menu-item-edit-active .menu-item-settings { overflow: hidden !important; } .cmb2_select { font-family: "nav"; } #tagsdiv-link_category { display: none; } </style>';
 	}
 
 	function wpdocs_custom_excerpt_length( $length ) {
