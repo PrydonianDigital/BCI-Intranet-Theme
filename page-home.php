be51d46a47cc81d60cff6374e5c6cc82107132d1
vbloke <vbloke@gmail.com>
2016-10-06 11:50:48 +0100

    removed potm from carousel

--- page-home.php
+++ page-home.php
@@ -80,84 +80,24 @@
 			</div>
 
 			<?php
-			$defaults = [
-				'fields'				 => 'ids',
-				'update_post_term_cache' => false,
-				'update_post_meta_cache' => false,
-				'cache_results'		  => false
-			];
-			$args = [
+			$args = array(
 				'post_type'	  => 'potw',
-				'posts_per_page' => '1',
-			];
-			$args1 = [
-				'post_type'	  => 'potm',
-				'posts_per_page' => '1',
-			];
-//			$stickies = get_option( 'sticky_posts' );
-//			if ( $stickies ) {
-//				$args3 = [
-//					'post_type'				=> 'post',
-//					'post__in'				=> $stickies,
-//					'ignore_sticky_posts'	=> 1
-//				];
-//			}
-			$potw_query = get_posts( array_merge( $defaults, $args  ) );
-			$potm_query = get_posts( array_merge( $defaults, $args1 ) );
-			//$news_query = get_posts( array_merge( $defaults, $args3 ) );
-			$post_ids = array_merge ( $potw_query, $potm_query ); //. You can swop around here
-			if ( $post_ids ) {
-				$final_args = [
-					'post_type' => ['potw', 'potm'],
-					'post__in'  => $post_ids,
-					'orderby'   => 'post__in', // If you need to keep the order from $post_ids
-					'order'	 => 'ASC' // If you need to keep the order from $post_ids
-				];
-				$loop = new WP_Query( $final_args );
-				if ( $loop->have_posts() ) : ?>
+				'posts_per_page' => '3',
+			);
+			$loop = new WP_Query( $args );
+			if ( $loop->have_posts() ) : ?>
 			<div class="row">
 				<div class="large-12 small-12 columns">
-					<h4 class="home-category featured">Featured</h4>
+					<h4 class="home-category featured">Picture of the Week</h4>
 					<div class="featured owl-carousel" id="featured-slider">
 					<?php while ( $loop->have_posts() ) : $loop->the_post();
 					$url = wp_get_attachment_url( get_post_thumbnail_id($post->ID) );
 				?>
 								<article class="item large-12 small-12 columns" style="background-image: url(<?php echo $url; ?>)">
 									<div>
-									<h4>
-										<?php
-											$post_type = get_post_type( $post->ID );
-											switch( $post_type ) {
-												case 'potw':
-													echo 'Picture of the Week';
-												break;
-												case 'potm':
-													echo 'Paper of the Month';
-												break;
-												default:
-													echo '';
-												break;
-											}
-										?>
-									</h4>
 									<h2><?php the_title(); ?></h2>
 									<?php the_excerpt(); ?>
-									<a class="readmore" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-										<?php
-											$post_type = get_post_type( $post->ID );
-											switch( $post_type ) {
-												case 'potw':
-													echo '<i class="nav-file-photo-o"></i> View Picture';
-												break;
-												case 'potm':
-													echo '<i class="nav-file-text-o"></i> View Paper';
-												break;
-												default:
-													echo 'Read More';
-												break;
-											}
-										?>
-									</a>
+									<a class="readmore" href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><i class="nav-file-photo-o"></i> View Picture</a>
 									</div>
 								</article>
 				<?php
@@ -168,7 +108,6 @@
 					endif;
 
 					wp_reset_postdata();
-				}
 				?>
 				</div>
 			</div>
