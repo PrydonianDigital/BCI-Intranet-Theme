9f314a4ded530cbe43c50b94c471aba0ff4be056
vbloke <vbloke@gmail.com>
2016-10-27 19:37:00 +0100

    login tweaks

--- functions.php
+++ functions.php
@@ -27,57 +27,6 @@
 	require_once('functions/lessc.inc.php');
 	require_once('icons.php');
 
-	// Log In
-	add_action( 'wp_login_failed', 'pu_login_failed' ); // hook failed login
-	function pu_login_failed( $user ) {
-	  	// check what page the login attempt is coming from
-	  	$referrer = $_SERVER['HTTP_REFERER'];
-
-	  	// check that were not on the default login page
-		if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') && $user!=null ) {
-			// make sure we don't already have a failed login attempt
-			if ( !strstr($referrer, '?login=failed' )) {
-				// Redirect to the login page and append a querystring of login failed
-				wp_redirect( $referrer . '?login=failed');
-			} else {
-			  	wp_redirect( $referrer );
-			}
-
-			exit;
-		}
-	}
-	add_action( 'authenticate', 'pu_blank_login');
-	function pu_blank_login( $user ) {
-	  	// check what page the login attempt is coming from
-	  	$referrer = $_SERVER['HTTP_REFERER'];
-
-	  	$error = false;
-
-	  	if($_POST['log'] == '' || $_POST['pwd'] == '') {
-	  		$error = true;
-	  	}
-
-	  	// check that were not on the default login page
-	  	if ( !empty($referrer) && !strstr($referrer,'wp-login') && !strstr($referrer,'wp-admin') && $error ) {
-
-	  		// make sure we don't already have a failed login attempt
-			if ( !strstr($referrer, '?login=failed') ) {
-				// Redirect to the login page and append a querystring of login failed
-				wp_redirect( $referrer . '?login=failed' );
-		  	} else {
-				wp_redirect( $referrer );
-		  	}
-
-		exit;
-
-	  	}
-	}
-
-	add_action('login_init', 'acme_autocomplete_login_init');
-	function acme_autocomplete_login_init() {
-	    ob_start();
-	}
-
 	add_action('login_form', 'acme_autocomplete_login_form');
 	function acme_autocomplete_login_form() {
 	    $content = ob_get_contents();
