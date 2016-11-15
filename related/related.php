260d6dcffcf0ddcde3262f2d8f7ac37820802595
vbloke <vbloke@gmail.com>
2016-11-15 17:07:34 +0000

    updated related section

--- related/related.php
+++ related/related.php
@@ -27,20 +27,20 @@
 		echo '</ul>';
 		echo '</div>';
 	}
-	$centres = Post_Connector::API()->get_children( 'centre-to-link', get_the_ID() );
-	if ( ! empty( $centres ) ) {
-		echo '<div class="small-12 large-6 column">';
-		echo '<h3>Related Links</h3>';
-		echo '<ul class="links">';
-		foreach( $centres as $centre ) {
-			$link = get_post_meta($centres->ID, 'link_url', true);
-			echo '<li>';
-			echo '<a href="'. $link . '">'. $centres->post_title . '</a>';
-			echo '</li>';
-		}
-		echo '</ul>';
-		echo '</div>';
-	}
+//	$centres = Post_Connector::API()->get_children( 'centre-to-link', get_the_ID() );
+//	if ( ! empty( $centres ) ) {
+//		echo '<div class="small-12 large-6 column">';
+//		echo '<h3>Related Links</h3>';
+//		echo '<ul class="links">';
+//		foreach( $centres as $centre ) {
+//			$link = get_post_meta($centres->ID, 'link_url', true);
+//			echo '<li>';
+//			echo '<a href="'. $link . '">'. $centres->post_title . '</a>';
+//			echo '</li>';
+//		}
+//		echo '</ul>';
+//		echo '</div>';
+//	}
 	$events = Post_Connector::API()->get_children( 'event-to-link', get_the_ID() );
 	if ( ! empty( $events ) ) {
 		echo '<div class="small-12 large-6 column">';
